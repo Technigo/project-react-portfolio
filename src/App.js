de29116a48990bfde3c10c35da8331c05a98cc2a
LinneaAjger <linnea.ajger@gmail.com>
2022-10-17 21:03:28 +0200

    created components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Projects } from 'components/projects'
+import { Projects } from 'components/Projects'
 
 export const App = () => {
   return (
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from 'react'
+import { API_TOKEN } from 'secret'
 
 export const Projects = () => {
   const [projectsList, setProjectsList] = useState([])
 
   const FetchProjects = () => {
-    fetch('https://api.github.com/users/linneaajger/repos')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: { API_TOKEN }
+      }
+    }
+
+    fetch('https://api.github.com/users/linneaajger/repos', options) // options object is passed as 2nd argument to fetch() function.
       .then((res) => res.json())
       .then((data) => {
         console.log(data)
         setProjectsList(data)
       })
   }
+
   useEffect(() => {
     FetchProjects()
   }, [])
