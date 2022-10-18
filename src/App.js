6bc91a9a6e8bf1f2ea2bc24c23c5c05294e65bc9
Sneezan <ceciliafraanlund@gmail.com>
2022-10-18 12:05:20 +0200

    added styles to header component and rendered in Appjs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import { Header, Title, Name } from 'styledcomponents/Header.js'
+import './index.css';
+
 export const App = () => {
   return (
     <div>
+      <Header>
+        <Name> Cecilia Frånlund</Name>
+        <Title> frontend developer </Title>
+      </Header>
       Find me in src/app.js!
     </div>
   )
---
+++ src/styledcomponents/Header.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components/macro'
+
+export const Header = styled.header`
+  background-color: #a19595;
+  width: 100%;
+  height: 70vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  position: relative;
+`;
+
+export const Title = styled.h1`
+    position: absolute;
+    width: 300px;
+    top: 36%;
+    left: 10%;
+    color: white;
+    font-size: 2.8rem;
+    line-height: 2.4rem;
+    font-family: roboto;
+`;
+
+export const Name = styled.h3`
+    font-weight: 700;
+    position: absolute;
+    top: 33%;
+    left: 10%;
+    font-size: 18px;
+    line-height: 18px;
+    text-transform: uppercase;
+    letter-spacing: 0.04em;
+    color: red;
+`
