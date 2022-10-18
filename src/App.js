3451376dd7f9d6e5d56d9cae3f8902a074f2e1f8
Charlotte Johansson <charlotte@verismohr.com>
2022-10-18 21:24:32 +0200

    started with json-file, created styled component for header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { CurrentTitle, Header, Name, Title } from 'styledComponents/Header';
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Header>
+      <Name>PORTFOLIO: CHARLOTTE JOHANSSON</Name>
+      <Title>frontend developer</Title>
+      <CurrentTitle>+ project manager</CurrentTitle>
+    </Header>
   )
 }
