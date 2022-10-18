62eb197e7aa3daa7c680e989c0a0c81e8836d97f
ViktorSvenssonN <viktor.tommy.svensson@gmail.com>
2022-10-18 21:35:17 +0200

    added img to AboutME

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,12 +1,13 @@
 /* eslint-disable max-len */
 import React from 'react';
 import styled from 'styled-components';
+import startFigure from './images/startFigure.png';
 
 const AboutMe = () => {
   return (
     <AboutMeContainer>
       <AboutMeBox>
-        <AboutMeParagraph>Hello World and people in it, I’m Viktor. A computer-loving gamer turned adult, turned store manager, turned sales support and product planner,
+        <AboutMeParagraph><span><img src={startFigure} className="AboutMEImg" alt="small logo that points to text" /> </span>Hello World and people in it, I’m Viktor. A computer-loving gamer turned adult, turned store manager, turned sales support and product planner,
              turned… drumroll… frontend developer! Hi *kind wave*. Cruising the waves of css and html, java’s and react’s and not regretting going full computer-lover full-circle at 34, one bit.
               I’ve always been about making peoples days a little better whether it’s behind a cashier or in a support call, doing it with awesome applications and websites is even better.
         </AboutMeParagraph>
