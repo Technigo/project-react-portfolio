9995d53e4ac4144d07f22950f8aefbb7d7ac14c1
jessikalind <jessika.lind@me.com>
2022-10-18 17:50:45 +0200

    articles array moved to own folder and file

--- src/Components/MyThoughts.js
+++ src/Components/MyThoughts.js
@@ -2,24 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 import { OuterWrapper, InnerWrapper, P, A } from '../Styled components/GlobalStyles'
 import { WhitePinkH2 } from '../Styled components/HeadlineStyles'
+import { MediumArticles } from '../Data/MediumAricles'
 
 const MyThoughts = () => {
-// An array for my articles in Medium (to be updated)
-  const Articles = [
-    {
-      name: 'Visual Learning (Or Just Going Crazy?',
-      url: 'https://medium.com/@jessika.lind/visual-learning-or-just-going-crazy-8bed5ea03163',
-      intro: 'If you Google the word “Visualization”, you are presented with a whole lot of different information. Everything from inspiring quotes (“You can reach that goal, just visualize it!”) to instructions on memory techniques. Let me tell you what “Visualization” means to me — eight weeks into the Technigo Web Development Bootcamp.',
-      date: '2022-10-16'
-    }
-  ]
-
   return (
     <OuterWrapper>
       <InnerWrapper>
         <WhitePinkH2>MY THOUGHTS</WhitePinkH2>
 
-        {Articles.map((article) => {
+        {MediumArticles.map((article) => {
           return (
             <div>
               <Date>{article.date}</Date>
---
+++ src/Data/MediumAricles.js
@@ -0,0 +1,8 @@
+export const MediumArticles = [
+  {
+    name: 'Visual Learning (Or Just Going Crazy?',
+    url: 'https://medium.com/@jessika.lind/visual-learning-or-just-going-crazy-8bed5ea03163',
+    intro: 'If you Google the word “Visualization”, you are presented with a whole lot of different information. Everything from inspiring quotes (“You can reach that goal, just visualize it!”) to instructions on memory techniques. Let me tell you what “Visualization” means to me — eight weeks into the Technigo Web Development Bootcamp.',
+    date: '2022-10-16'
+  }
+]
