e9b75a8d85438f9c7f0f598ecbbddef46a4f8128
Linda Malm <Linda@Lindas-MacBook-Pro.local>
2022-10-19 19:13:12 +0200

    Got a working fetch to my git-repositorys going.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import { InnerWrapper, /* Devices, */MainHeader, MainSections } from 'styles/mainStyles';
+import React, { useState, useEffect } from 'react';
+import { BASE_URL } from 'utils/urls';
+import { InnerWrapper, /* Devices, */MainHeader, MainSections, MainText } from 'styles/mainStyles';
 
 const Projects = () => {
+  const [loading, setLoading] = useState(false);
+  const [list, setList] = useState({});
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(BASE_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('data', data)
+        return setList(data)
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        console.log('list in fetch', list)
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return (
+      <p>Page is loading...</p>
+    )
+  }
+
   return (
     <MainSections coloredBackground>
       <InnerWrapper>
         <MainHeader>FEATURED PROJECTS</MainHeader>
+        <MainText>{console.log('list', list)}</MainText>
       </InnerWrapper>
     </MainSections>
   )
