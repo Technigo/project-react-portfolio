5a0105362478f9f947e24bad17c0795391466e2f
SofieRydmark <you@example.com>
2022-10-19 16:12:27 +0200

    more thoughts section

--- src/components/MoreThoughts/MoreThoughts.js
+++ src/components/MoreThoughts/MoreThoughts.js
@@ -1,11 +1,19 @@
 /* eslint-disable linebreak-style */
 import React from 'react'
 import { ContainerGrey, SectionHeadings } from 'Globalstyles';
+import { StyledMoreThoughts } from './MoreThoughts.styles';
 
 const MoreThoughts = () => {
   return (
     <ContainerGrey>
       <SectionHeadings>MORE THOUGHTS</SectionHeadings>
+      <StyledMoreThoughts>
+        <a href="https://medium.com/codesphere-cloud/10-algorithms-every-developer-should-learn-628faf0f9234" role="button" aria-pressed="false" aria-label="my thoughts" target="_blank" rel="noreferrer">
+          <p><span style={{ fontWeight: 700 }}>SEPT 2021.</span>
+          10 Algorithms Every Developer Should Learn <span style={{ color: '#829460' }}>&#x276F;&#x276F;</span>
+          </p>
+        </a>
+      </StyledMoreThoughts>
     </ContainerGrey>
   )
 }
---
+++ src/components/MoreThoughts/MoreThoughts.styles.js
@@ -0,0 +1,25 @@
+/* eslint-disable linebreak-style */
+import styled from 'styled-components'
+
+export const StyledMoreThoughts = styled.div`
+    text-align: center;
+
+    p {
+        font-family: 'Roboto';
+        line-height: 1.81em;
+        font-style: normal;
+        font-weight: 400;
+        letter-spacing: 0.01em;
+        color: black;
+        font-size: 1.1em;
+    }
+
+    a {
+        text-decoration: none;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+
+`
