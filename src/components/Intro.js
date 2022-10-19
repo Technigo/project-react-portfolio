045367a5d13e3925d409180de182dcb61958d4cd
miadahlgren <101955676+miadahlgren@users.noreply.github.com>
2022-10-19 16:08:55 +0200

    new files

---
+++ src/components/Intro.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-lone-blocks */
+/* eslint-disable max-len */
+import React from 'react';
+import Indent from 'icons/indent.png'
+
+const Intro = () => {
+  return (
+    <p lang="en"><img src={Indent} alt="start symbol" className="indent" />Hello! Welcome to my portfolio.
+            My name is Mia Dahlgren and I am currently a student at <a href="https://www.technigo.io/" alt="link to technigo website">Technigo Web Development Bootcamp</a>
+            (Class of fall 2022). Take a look further down to see what I have accomplished during the last weeks.
+            Please dont hesitate to reach out if you are curious to know more!
+    </p>
+  );
+}
+export default Intro
+
+{ /* <section class="section intro">
+        <!--inner wrapper -> intro section -->
+        <!--here goes short introduction about me -->
+        <h2 lang="en">Introduction</h2>
+        <p lang="en"><img src="./icons/indent.png" alt="start symbol" class="indent"></img>Hello! Welcome to my portfolio.
+            My name is Mia Dahlgren and I am currently a student at <a href="https://www.technigo.io/" alt="link to technigo website">Technigo Web Development Bootcamp</a>
+            (Class of fall 2022). Take a look further down to see what I have accomplished during the last weeks.
+            Please don't hesitate to reach out if you are curious to know more! </p>
+        </section>
+
+        */ }
