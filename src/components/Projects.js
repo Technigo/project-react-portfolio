14a1c6895b72a32857d50a4855497192d8df5234
Linda Malm <Linda@Lindas-MacBook-Pro.local>
2022-10-20 00:02:22 +0200

    Got the github filtering to work.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,40 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import { BASE_URL } from 'utils/urls';
+// import styled from 'styled-components/macro';
 import { InnerWrapper, /* Devices, */MainHeader, MainSections, MainText } from 'styles/mainStyles';
 
 const Projects = () => {
   const [loading, setLoading] = useState(false);
   const [list, setList] = useState({});
+  const [filteredList, setFilteredList] = useState({});
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     fetch(BASE_URL)
       .then((response) => response.json())
       .then((data) => {
-        console.log('data', data)
-        return setList(data)
+        setList(data);
+        const listItems = [];
+        list.map((item) => {
+          if (item.fork === true && item.name.includes('project')) {
+            listItems.push(item);
+          }
+          return 0;
+        });
+        setFilteredList(listItems);
       })
       .catch((error) => console.error(error))
-      .finally(() => {
-        console.log('list in fetch', list)
-        setLoading(false)
-      })
-  }, [])
+      .finally(() => setLoading(false));
+  }, []);
 
   if (loading) {
     return (
       <p>Page is loading...</p>
-    )
+    );
   }
 
   return (
     <MainSections coloredBackground>
       <InnerWrapper>
         <MainHeader>FEATURED PROJECTS</MainHeader>
-        <MainText>{console.log('list', list)}</MainText>
+        {filteredList.map((project) => {
+          return (
+            <MainText>{project.name}</MainText>
+          );
+        })}
       </InnerWrapper>
     </MainSections>
-  )
+  );
 }
 
 export default Projects;
+
+// const ProjectImage = styled.img`
+//   opacity: 1.0;
+//   width: 300px;
+//   padding: 8% 5%;
+// `
