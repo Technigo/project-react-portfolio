ed17ad0bd862cd1b86d821cb4735e338e60d0314
Scarylie <saralie.bognandi@gmail.com>
2022-10-20 15:18:33 +0200

    header, style and Icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,95 @@
 import React from 'react';
 import styled from 'styled-components';
-import { BlockSection, InnerWrapper } from './StyledComponents/Wrappers';
-// import backgroundImg from './src/pic/forest.jpeg';
+import { InnerWrapper } from './StyledComponents/Wrappers';
+import backgroundImg from '../assets/forest.jpeg';
+import profileImg from '../assets/sar.bild.jpg';
+import { IconLinks } from './StyledComponents/IconLinks';
 
 export const Header = () => {
   return (
     <HeaderBackground>
-      <BlockSection>
-        <InnerWrapper>Header</InnerWrapper>
-      </BlockSection>
+      {/* <BlockSection> */}
+      <InnerWrapper>
+        <HeaderImagesWrapper>
+          <IconLinks />
+          <ProfileImage src={profileImg} alt="Saralie Bognandi" />
+        </HeaderImagesWrapper>
+        <HeaderInfo>
+          <p>
+            Portfolio: <span>Saralie Bognandi</span>
+          </p>
+          <h1>frontend developer</h1>
+          <h2>
+            <span>& design engineer</span>
+          </h2>
+        </HeaderInfo>
+      </InnerWrapper>
+      {/* </BlockSection> */}
     </HeaderBackground>
   );
 };
 
-const HeaderBackground = styled.div`
-  // background-image: url ();
+const HeaderBackground = styled.header`
+  position: relative;
+  background-image: linear-gradient(
+      rgba(11, 11, 11, 0.52),
+      rgba(11, 11, 11, 0.52)
+    ),
+    url(${backgroundImg});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 45vh;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    height: 50vh;
+  }
+  @media (min-width: 1024px) {
+    height: 60vh;
+  }
+`;
+
+const HeaderInfo = styled.div`
+  position: absolute;
+  left: 0;
+  top: 5em;
+  color: white;
+  font-size: 1.4em;
+  max-width: 70%;
+
+  h1 {
+    color: pink;
+  }
+  h2 {
+  }
+  p {
+  }
+  span {
+    font-weight: lighter;
+  }
+  @media (min-width: 768px) {
+    font-size: 2em;
+  }
+  @media (min-width: 1024px) {
+    font-size: 2.2em;
+  }
+`;
+const HeaderImagesWrapper = styled.div``;
+
+const ProfileImage = styled.img`
+  width: 10em;
+  max-width: 200px;
+  border: 2px solid white;
+  border-radius: 50%;
+  position: absolute;
+  right: 0;
+  bottom: -2rem;
+
+  @media (min-width: 768px) {
+    width: 15em;
+  }
+  @media (min-width: 1024px) {
+    width: 20em;
+  }
 `;
--- src/components/StyledComponents/Wrappers.js
+++ src/components/StyledComponents/Wrappers.js
@@ -3,11 +3,15 @@ import styled from 'styled-components';
 export const BlockSection = styled.div`
   padding: 50px 0;
   background: pink;
+
+  & h1 {
+  }
 `;
 
 export const InnerWrapper = styled.div`
   width: 80%;
+  height: inherit;
   max-width: 700px;
   margin: 0 auto;
-  background: white;
+  position: relative;
 `;
