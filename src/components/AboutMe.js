af356b98065e9d351ac34398f375ce4673122eb7
Scarylie <saralie.bognandi@gmail.com>
2022-10-20 09:58:49 +0200

    About me, info Line and dot

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import { BlockSection, InnerWrapper } from './StyledComponents/Wrappers';
 
 export const AboutMe = () => {
-  return <div>About me</div>;
+  return (
+    <BlockSection>
+      <InnerWrapper>
+        <p>
+          <Line />
+          <Dot /> Hello, I am a student at Technigo and prosper to become a web
+          developer. Currently working as a mechanical engineer. Welcome to my
+          portfolio where you can follow my projects and journey.
+        </p>
+      </InnerWrapper>
+    </BlockSection>
+  );
 };
+
+const Line = styled.span`
+  border: 1px solid #fa382f;
+  width: 40px;
+  display: inline-block;
+  margin-bottom: 4px;
+`;
+const Dot = styled.span`
+  height: 10px;
+  width: 10px;
+  background-color: #fa382f;
+  border-radius: 50%;
+  display: inline-block;
+`;
