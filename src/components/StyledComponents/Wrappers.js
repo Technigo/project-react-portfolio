bb918ca984a31be05065d1813df8be184e8dbcdf
Scarylie <saralie.bognandi@gmail.com>
2022-10-20 20:56:47 +0200

    styles

--- src/components/StyledComponents/Wrappers.js
+++ src/components/StyledComponents/Wrappers.js
@@ -14,4 +14,25 @@ export const InnerWrapper = styled.div`
   max-width: 700px;
   margin: 0 auto;
   position: relative;
+
+  h3 {
+    background-color: #fa382f;
+    text-transform: uppercase;
+    color: white;
+    font-weight: 700;
+    width: fit-content;
+    margin: 0 auto 30px;
+  }
+
+  h4 {
+    color: #fa382f;
+    text-transform: uppercase;
+    margin: 0;
+    font-size: 17px;
+    text-align: center;
+  }
+
+  p {
+    text-align: left;
+  }
 `;
