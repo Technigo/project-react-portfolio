c1005dc2889211765d2a1e26adf51ad494318cb3
Scarylie <saralie.bognandi@gmail.com>
2022-10-20 23:13:36 +0200

    style font sized

--- src/components/StyledComponents/Wrappers.js
+++ src/components/StyledComponents/Wrappers.js
@@ -22,17 +22,35 @@ export const InnerWrapper = styled.div`
     font-weight: 700;
     width: fit-content;
     margin: 0 auto 30px;
+    font-size: 2.4rem;
   }
 
   h4 {
     color: #fa382f;
     text-transform: uppercase;
     margin: 0;
-    font-size: 17px;
+    font-size: 2.2rem;
     text-align: center;
   }
 
+  h5 {
+    font-size: 2rem;
+    color: #fa382f;
+    text-transform: uppercase;
+  }
+
+  h6 {
+    font-size: 1.8rem;
+  }
+
   p {
-    text-align: left;
+    font-size: 1.5rem;
+  }
+
+  a {
+    font-size: 1.5rem;
+  }
+  a:link {
+    text-decoration: none;
   }
 `;
