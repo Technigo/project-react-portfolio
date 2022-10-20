f6b1d0db8d686c39d67220e08e029661538065ac
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2022-10-20 17:55:13 +0200

    addition

--- src/components/OtherThoughts.js
+++ src/components/OtherThoughts.js
@@ -4,6 +4,7 @@ import React from 'react';
 const About = () => {
 	return (
 		<div>
+			<h3>my top 3 websites for learning coding</h3>
 			<h4>w3schools</h4>
 			<a href="https://www.w3schools.com/" target="_blank" rel="noopener noreferrer">www.w3schools.com
 			</a>
