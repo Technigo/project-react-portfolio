a2ee0b859718250a1e6104a34814b9f06314d916
Maria Westling <maria_westling@hotmail.com>
2022-10-21 14:50:54 +0200

    working with projects

--- src/components/FeaturedProjects.js
+++ src/components/FeaturedProjects.js
@@ -27,15 +27,16 @@ export const FeaturedProjects = () => {
         <FeaturedProjectsContent>
           {BigProjects.map((prop) => (
             <div key={prop.id}>
-              <ProjectLink href={prop.html_url} alt="Link to project" target="_blank">
-                <ImageAndName>
-                  <BigProjectsImage alt="project-img" src={`https://raw.githubusercontent.com/marwebdesign/${prop.name}/master/code/Thumbnail/thumbnail.png`} />
-                  <ProjectNameOverImage>{prop.name}</ProjectNameOverImage>
-                </ImageAndName>
-              </ProjectLink>
-              <ProjectsHeading>{prop.name}</ProjectsHeading>
-              <p>{prop.description}</p>
-              {/* <p>{prop.html_url}</p> */}
+              <FeaturedProjectsContainer>
+                <ProjectLink href={prop.html_url} alt="Link to project" target="_blank">
+                  <ImageAndName>
+                    <BigProjectsImage alt="project-img" src={`https://raw.githubusercontent.com/marwebdesign/${prop.name}/master/code/Thumbnail/thumbnail.png`} />
+                    <ProjectNameOverImage>{prop.name}</ProjectNameOverImage>
+                  </ImageAndName>
+                </ProjectLink>
+                <ProjectsHeading>{prop.name}</ProjectsHeading>
+                <p>{prop.description}</p>
+              </FeaturedProjectsContainer>
               {prop.topics.map((tag) => (
                 <Topics>{tag}</Topics>
               ))}
@@ -48,14 +49,37 @@ export const FeaturedProjects = () => {
 }
 
 export const FeaturedProjectsContent = styled.div`
-display: flex;
+display: grid;
+grid-template-columns: 1fr;
+/* display: flex;
 flex-direction: column;
-justify-content: flex-start;
+justify-content: flex-start; */
 /* align-items: center; */
 /* text-align: center; */
+/* @media (min-width: 1025px) {
+  grid-template-columns: repeat(2, 1fr);
+} */
 font-family:'Roboto', sans-serif;
 line-height: 30px;
+
+@media (min-width: 1025px) {
+grid-template-columns: repeat(2, 1fr);
+column-gap: 5%;
+  }
 `
+
+export const FeaturedProjectsContainer = styled.div`
+/* width: 100%; */
+display: flex;
+flex-direction: column;
+/* @media (min-width: 1025px) {
+  width: 40%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  } */
+
+`
+
 export const ImageAndName = styled.div`
 position: relative;
 `
@@ -69,10 +93,13 @@ opacity: 0.7;
   opacity: 1;
 }
 `
+
 export const ProjectNameOverImage = styled.h2`
+text-transform: uppercase;
 position: absolute;
 background-color: #D36B00;
 padding: 0px 4px;
+display: inline;
 font-size: 22px;
 border-radius: 5px;
 top: 50%;
@@ -85,13 +112,13 @@ transform: translate(-50%, -50%);
 
 export const ProjectLink = styled.a`
 color: white;
-
-
 &: :visited {
 color:white;
 }
 `
 
 export const ProjectsHeading = styled.h4`
+text-transform: uppercase;
 color: #D36B00;
 `
+
