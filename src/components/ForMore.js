2795415649a13bc7a2e87726a3778413bb7e57ea
Annika Gadman <annika.gadman@gmail.com>
2022-10-20 23:50:10 -0500

    changed icons structure

--- src/components/ForMore.js
+++ src/components/ForMore.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import styled from 'styled-components/macro'
-import { InnerWrapper, OuterWrapper, SectionTitle, Icons } from './global/GlobalStyling';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+import { InnerWrapper, OuterWrapper, SectionTitle } from './global/GlobalStyling';
 
 export const ForMore = () => {
   const ForMoreSection = styled.div`
     text-align: center;
     padding: 50px 0;
   `
-  const SocialMediaLinks = styled.div`
-    width: 100px;
-    height: 100px
-    background-color: pink;
-  `
+  const IconWrapper = styled.div`
+        display: flex;
+        flex-direction: row;
+        padding: 20px;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+`
+  const IconContainer = styled.div`
+    margin: 20px;
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
+    &:hover {
+        transform: scale(1.1);
+    }
+    `
   return (
     <OuterWrapper>
       <InnerWrapper>
@@ -19,9 +32,38 @@ export const ForMore = () => {
           <SectionTitle>
             <p>for more</p>
           </SectionTitle>
-          <SocialMediaLinks>
-            <Icons />
-          </SocialMediaLinks>
+          <IconWrapper>
+            <IconContainer>
+              <a
+                href="https://github.com/"
+                target="_blank"
+                rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} size="3x" color="#212427" />
+              </a>
+              <p>Github</p>
+            </IconContainer>
+            <IconContainer>
+              <a
+                href="https://www.linkedin.com/in/lovisa-carling/"
+                target="_blank"
+                rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} size="3x" color="#212427" />
+              </a>
+              <p>LinkedIn</p>
+            </IconContainer>
+            <IconContainer>
+              <a
+                href="https://stackoverflow.com/users/16687109/asivol"
+                target="_blank"
+                rel="noopener noreferrer">
+                <FontAwesomeIcon
+                  icon={faStackOverflow}
+                  size="3x"
+                  color="#212427" />
+              </a>
+              <p>Stackoverflow</p>
+            </IconContainer>
+          </IconWrapper>
         </ForMoreSection>
       </InnerWrapper>
     </OuterWrapper>
--- src/components/global/GlobalStyling.js
+++ src/components/global/GlobalStyling.js
@@ -1,11 +1,4 @@
-import React from 'react';
 import styled from 'styled-components/macro'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faGithub,
-  faLinkedin,
-  faStackOverflow
-} from '@fortawesome/free-brands-svg-icons'
 
 export const OuterWrapper = styled.div`
     width: 100%;
@@ -49,54 +42,3 @@ export const Tag = styled.div`
     margin: 3px;
   }
 `
-export const Icons = () => {
-  const IconWrapper = styled.div`
-        display: flex;
-        flex-direction: row;
-        padding: 20px;
-        flex-wrap: wrap;
-        justify-content: space-evenly;
-`
-  const IconContainer = styled.div`
-    margin: 20px;
-    transition: all 0.2s ease-in-out;
-    cursor: pointer;
-    &:hover {
-        transform: scale(1.1);
-    }
-    `
-  return (
-    <IconWrapper>
-      <IconContainer>
-        <a
-          href="https://github.com/"
-          target="_blank"
-          rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faGithub} size="3x" color="#212427" />
-        </a>
-        <p>Github</p>
-      </IconContainer>
-      <IconContainer>
-        <a
-          href="https://www.linkedin.com/in/lovisa-carling/"
-          target="_blank"
-          rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faLinkedin} size="3x" color="#212427" />
-        </a>
-        <p>LinkedIn</p>
-      </IconContainer>
-      <IconContainer>
-        <a
-          href="https://stackoverflow.com/users/16687109/asivol"
-          target="_blank"
-          rel="noopener noreferrer">
-          <FontAwesomeIcon
-            icon={faStackOverflow}
-            size="3x"
-            color="#212427" />
-        </a>
-        <p>Stackoverflow</p>
-      </IconContainer>
-    </IconWrapper>
-  )
-};
