b99e2d600ced834f61118e89b037f1155d36d74a
KajaWilbik <wilbik.kaja@gmail.com>
2022-10-21 19:46:40 +0200

    last changes

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import ProjectPicture from '../Pictures/ProjectPicture.jpg'
 
 export const ProjectBox = ({ list }) => {
   const [project, setProject] = useState([]);
@@ -17,21 +18,107 @@ export const ProjectBox = ({ list }) => {
   console.log('everything is fine in fetch', project)
 
   return (
-    project.map((projects) => {
+    // filtered and maped only forked projects from Github
+    project.filter((projects) => projects.fork === true && projects.name.includes('project')).map((filteredProjects) => {
       return (
-        <div key={projects.full_name}>
-          <h2>{projects.name}</h2>
-          <p>{projects.description}</p>
-        </div>
+        <StyledProjectBox className="grid" key={filteredProjects.full_name}>
+
+          <a
+            href={filteredProjects.html_url}
+            target="_blank"
+            rel="noreferrer">
+            <h2>{filteredProjects.name}</h2>
+            <img src={ProjectPicture} alt="coding" />
+            <p>{filteredProjects.description}</p>
+            <div className="tags">
+              {/* fetched tags from Github. First => make a changes in githib in each project */}
+              <p>{filteredProjects.topics[0]}</p>
+              <p>{filteredProjects.topics[1]}</p>
+              <p>{filteredProjects.topics[2]}</p>
+            </div>
+
+          </a>
+
+        </StyledProjectBox>
 
       )
     })
   )
 }
 
-// {repository.map((project) => {
-//     return(
-//         <div key={project.id}>
-//         {project}
-//         </div>
+const StyledProjectBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    padding: 20px;
+    align-items: center;
 
+    a {
+      text-decoration:none;
+      color: black;
+    }
+    
+    a:hover {
+      color:violet;
+    }
+
+    img {
+    padding-top: 10px;
+    width: 100%;
+    }
+
+    .tags {
+      font-family: "Roboto", sans-serif;
+      font-weight: bold;
+      display: inline-flex;
+      padding: 0.5rem;
+
+
+
+    }
+
+    .tags p {
+
+      background-color: #7b9f9b;
+      text-decoration: none;
+  
+       /* Matching line-height and font-size here gives full control 
+       over the size of our tags to the padding*/
+      font-size: 1rem; 
+      line-height: 15px;
+  
+      /* Control the size of the tags */
+      padding: 4px;
+  
+      /* Control the spacing of our tags */
+      margin: 3px;
+  
+      /* Some rounded borders */
+      border-radius: 5px;
+    }
+
+
+    @media screen and (min-width: 1024px) {
+      margin: 20px;
+display: flex;
+flex-direction:column;
+align-items: center;
+text-align: center;
+
+    h2 {
+      text-align: center;
+      font-size: 35px;
+    }
+
+    p {
+      font-size: 30px;
+    }
+    
+    img {
+      width: 50%;
+      height: 50%;
+      align-content:center;
+    }
+  }
+
+`
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,39 +3,45 @@ import styled from 'styled-components';
 
 export const Skills = () => {
   return (
-    <StyledSkills>
-      <div className="code">
-        <ul>
-          <li>HTML</li>
-          <li>CSS3</li>
-          <li>JavaScript ES6</li>
-          <li>React</li>
-          <li>Styled Components</li>
-          <li>GitHub</li>
-        </ul>
-      </div>
-      <div className="toolbox">
-        <ul>
-          <li>Atom</li>
-          <li>Postman</li>
-          <li>GIMP</li>
-          <li>Canva</li>
-          <li>Figma</li>
-          <li>Keynote</li>
-          <li>Slack</li>
-        </ul>
+    <>
+      <StyledSkills>
+        <div className="code">
+          <ul>
+            <li>HTML</li>
+            <li>CSS3</li>
+            <li>JavaScript ES6</li>
+            <li>React</li>
+            <li>Styled Components</li>
+            <li>GitHub</li>
+          </ul>
+        </div>
+        <div className="toolbox">
+          <ul>
+            <li>Atom*</li>
+            <li>Postman</li>
+            <li>GIMP</li>
+            <li>Canva</li>
+            <li>Figma</li>
+            <li>Keynote*</li>
+            <li>Slack</li>
+          </ul>
+
+        </div>
+        <div className="More">
+          <ul>
+            <li>Agile Methodology</li>
+            <li>Design Lead</li>
+            <li>Strategy*</li>
+            <li>Process design*</li>
+          </ul>
+
+        </div>
+      </StyledSkills>
+      <div className="notes">
+        <p>* skills will be acquire until the end of education</p>
 
       </div>
-      <div className="More">
-        <ul>
-          <li>Agile Methodology</li>
-          <li>Design Lead</li>
-          <li>Strategy</li>
-          <li>Process design</li>
-        </ul>
-
-      </div>
-    </StyledSkills>
+    </>
 
   )
 }
@@ -43,6 +49,10 @@ export const Skills = () => {
 const StyledSkills = styled.li`
 padding: 20px;
 display:inline-flex;
+
+.notes {
+    margin:20px;
+}
 
 @media screen and (min-width: 768px) {
     display: grid;
