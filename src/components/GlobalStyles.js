8a9c463438a58c9b264f9377e9100bc54d76b3d5
Sofia Andersson <sofia.e.i.andersson@gmail.com>
2022-10-22 18:52:21 +0200

    Footer done

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -16,12 +16,15 @@ body {
   }
 `;
 
+/*
 export const Wrapper = styled.div`
 display: flex;
 flex-direction: column;
 `;
+*/
 
 export const Header = styled.h2`
+font-family: 'Montserrat', sans-serif;
 font-size: 18px;
 color: white;
 margin-bottom: 5%;
