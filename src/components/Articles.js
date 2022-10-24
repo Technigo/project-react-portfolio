faf5123cd44745d53df4119f001dbbcfe9af8773
Scarylie <saralie.bognandi@gmail.com>
2022-10-24 16:16:07 +0200

    link and image

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { BlockSection, InnerWrapper } from './styles/Wrappers';
-import backgroundImg from '../assets/forest.jpeg';
+import articleImg from '../assets/article.jpeg';
 
 export const Articles = () => {
   return (
@@ -9,11 +9,17 @@ export const Articles = () => {
       <InnerWrapper>
         <h3>My thoughts about code</h3>
         <div>
-          <img src={backgroundImg} alt="project img" width="100%" />
           <Content>
-            <h5>Oct 17, 2022</h5>
-            <h6>My journey towards becoming a web developer</h6>
-            <a href="https://medium.com/@saralie.bognandi/my-journey-towards-becoming-a-web-developer-f7e42c31e522">
+            <a
+              href="https://medium.com/@saralie.bognandi/my-journey-towards-becoming-a-web-developer-f7e42c31e522"
+              target="_blank"
+              rel="noreferrer"
+              tabIndex={-1}
+              aria-hidden>
+              <img src={articleImg} alt="project img" />
+              <h5>Oct 17, 2022</h5>
+              <h6>My journey towards becoming a web developer</h6>
+
               <p>
                 In February I sent in my application to the online 20 week
                 bootcamp at Technigo to study web development. Our application
