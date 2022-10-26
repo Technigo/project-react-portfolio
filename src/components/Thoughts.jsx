1294c150ccff298abaa405e97dd31b85ea36f242
Fredrik Holm <karlfredrikholm@gmail.com>
2022-10-26 10:58:31 +0200

    changed heading on thoughts

--- src/components/Thoughts.jsx
+++ src/components/Thoughts.jsx
@@ -19,12 +19,12 @@ const Thoughts = () => {
             rel="noreferrer">
             <ThoughtsImage style={{ backgroundImage: `url(${thoughtImg})` }}>
               <div>
-                <h6>November 2022</h6>
+                <h6>October 2022</h6>
               </div>
             </ThoughtsImage>
           </a>
           <h5>Some personal gibberish</h5>
-          <Paragraph grid>Some personal gibberish about how
+          <Paragraph grid>About how
           I started learning web development.
           </Paragraph>
           <TopicsContainer thoughts>
@@ -45,9 +45,8 @@ const Thoughts = () => {
               </div>
             </ThoughtsImage>
           </a>
-          <h5>Coming Soon</h5>
-          <Paragraph grid>Some personal gibberish about something
-          else.
+          <h5>Some personal gibberish</h5>
+          <Paragraph grid>About 5 things you must NOT know to be a developer in 2022.
           </Paragraph>
           <TopicsContainer thoughts>
             <a
