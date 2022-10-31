eedfa1aa579fe986295bb85ebcf2fe89d5144d61
KajaWilbik <wilbik.kaja@gmail.com>
2022-10-31 16:35:27 +0100

    Updated pitch

--- src/components/Pitch.js
+++ src/components/Pitch.js
@@ -4,17 +4,11 @@ import styled from 'styled-components';
 export const Pitch = () => {
   return (
     <StyledPitch>
-      <h3>I am a people-oriented person with a strong background in tourism and recreation.
-        I have a solid knowledge of customer service and building long-term relations with clients.
-        My previous experience taught me how to handle many tasks
-        at the same time, as well as plan and handle stressful situations.
-        I am a self-driven, responsible, and punctual person with a strong learning desire
-        and the ability to work alone or as a part of a team, and strong interpersonal skills.
-        I have excellent time-management and planning skills.
-        In addition, I am a swift learner with ease in learning languages, currently proficient in:
-        Polish (native speaker),English (Professional working proficiency),
-         and Swedish (Professional working proficiency).
-        In my free time,  I am learning as well Spanish.
+      <h3>I am a frontend developer and a people-oriented person with a strong background in tourism and recreation changing my career into TECH.
+
+I am a self-driven, responsible, and punctual person with a strong learning desire and the ability to work alone or as a part of a team, and strong interpersonal skills. I am proficient at public speaking and I have excellent time-management and planning skills.
+
+I have experience in HTML, CSS, React JS and Redux, and I am learning Node.js. I have also previous experience working with WordPress, and Canva and basic knowledge of how to use GIMP. 
       </h3>
     </StyledPitch>
 
