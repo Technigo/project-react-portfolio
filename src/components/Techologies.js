016ae68b84b3b67884ea6152d1449b436cdb69c9
Scarylie <saralie.bognandi@gmail.com>
2022-11-01 21:09:59 +0100

    margin added

--- src/components/Techologies.js
+++ src/components/Techologies.js
@@ -1,12 +1,14 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import { BlockSectionWhite, InnerWrapper, TagWrapper } from './styles/Wrappers';
 
 export const Technologies = () => {
   return (
     <BlockSectionWhite>
       <InnerWrapper>
-        <h3>Tech</h3>
+        <Title>
+          <h3>Tech</h3>
+        </Title>
         <TagWrapper className="wrap-text">
           <p>HTML</p>
           <p>CSS</p>
@@ -29,3 +31,7 @@ export const Technologies = () => {
     </BlockSectionWhite>
   );
 };
+
+const Title = styled.div`
+  margin-bottom: 1em;
+`;
