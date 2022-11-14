9177a2563b4f8993ff31fe538ea837ce43166aa9
Linda Malm <Linda@Lindas-MBP.lan>
2022-11-14 11:00:05 +0100

    Updated the header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
         <HeaderLinks type="image" src={linksImg} onClick={linksBottom} name="links-at-the-bottom" alt="bottom-scroll" />
         <TitleName>PORTFOLIO: <BoldName>LINDA MALM</BoldName></TitleName>
         <TitleFirst>frontend developer</TitleFirst>
-        <TitleSecond>+ digital designer</TitleSecond>
+        <TitleSecond>+ visual storyteller</TitleSecond>
         <ProfileImg src={ProfileImage} alt="Linda-Malm" />
       </CloneWrapper>
     </Background>
