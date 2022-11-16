a69cd2cc06272ae3b0866977ec2fdd8bc6a59b23
Tina Bruce <tinabruce42@hotmail.com>
2022-11-16 21:25:40 +0100

    deleted duplicated folders and files

--- src/utils/urls.js
+++
@@ -1,3 +0,0 @@
-// All the fetch-links used for this app
-export const BASE_URL = 'https://api.github.com/users/brucette/repos';
-export const PROJECT_THUMBNAIL = (projectName) => `https://raw.githubusercontent.com/brucette/${projectName}/master/code/preview/projPreview.png`;
