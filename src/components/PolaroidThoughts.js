7b608e1d34b272bcef436cf33f244c4da88c1881
Annika Gadman <annika.gadman@gmail.com>
2022-12-02 10:43:46 -0600

    fixed responsive polaroids

--- src/components/PolaroidThoughts.js
+++ src/components/PolaroidThoughts.js
@@ -17,6 +17,10 @@ export const PolaroidTest = () => {
 }
 
 const PictureContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
   .polaroid {
     border: 1px solid darkgrey;
@@ -24,29 +28,18 @@ const PictureContainer = styled.div`
     padding: 15px 15px 60px 15px;
     box-shadow: 5px 10px 10px darkgrey;
     margin-bottom: 20px;
+    max-width: 320px;
   }
 
-  .polaroid:after {
-    content: attr(polaroid-caption);
-    position: absolute;
-    bottom: 0px;
-    left: 0px;
-    width: calc(100% - 20px);
-    padding: 10px;
-    font-size: 30px;
-    text-align: center;
-    font-family: 'Indie Flower', cursive;
+  @media (min-width: 753px) {
+    flex-direction: row;
+    justify-content: space-evenly;
   }
-
-  @media (max-width: 899px) {
-    display: flex;
-    flex-direction: column;
-  
-    }
 
   @media (min-width: 900px) {
     position: relative;
     height: 500px;
+    
     .polaroid {
       position: absolute;
       border: 1px solid darkgrey;
@@ -66,7 +59,7 @@ const PictureContainer = styled.div`
       text-align: center;
       font-family: 'Indie Flower', cursive;
     }
-
+    
     .image1 {
       left: 60px;
       top: 100px;
@@ -74,7 +67,7 @@ const PictureContainer = styled.div`
       -ms-transform: rotate(-10deg);
       -webkit-transform: rotate(-10deg);
     }
-
+    
     .image2 {
       left: 420px;
       top: 100px;
