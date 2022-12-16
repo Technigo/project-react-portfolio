52d282713cd07441f75b8a88cd4b11466c1b9598
katirum <kartikaprw@gmail.com>
2022-12-17 00:05:35 +0100

    added new json (skill json), added new project

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,45 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 import { SectionTitle, Wrapper, OverlinedHeading, ProjectText } from './GlobalStyles';
+import data from '../assets/skills.json'
 
 export const Skills = () => {
   return (
     <Wrapper primary>
       <SectionTitle primary>SKILLS</SectionTitle>
       <SkillsContainer>
-        <SkillsList>
-          <OverlinedHeading>CODE</OverlinedHeading>
-          <SkillName primary>JavaScript ES6</SkillName>
-          <SkillName primary>CSS3</SkillName>
-          <SkillName primary>HTML5</SkillName>
-          <SkillName primary>APIs</SkillName>
-          <SkillName primary>React</SkillName>
-          <SkillName primary>Redux</SkillName>
-          <SkillName primary>Express</SkillName>
-          <SkillName primary>MongoDB</SkillName>
-          <SkillName primary>Node JS</SkillName>
-          <SkillName primary>Styled Components</SkillName>
-        </SkillsList>
-        <SkillsList>
-          <OverlinedHeading>TOOLBOX</OverlinedHeading>
-          <SkillName primary>GitHub</SkillName>
-          <SkillName primary>VSCode</SkillName>
-          <SkillName primary>Figma</SkillName>
-          <SkillName primary>Canva</SkillName>
-          <SkillName primary>Slack</SkillName>
-          <SkillName primary>Netlify</SkillName>
-          <SkillName primary>Google Cloud</SkillName>
-          <SkillName primary>Stack Overflow</SkillName>
-        </SkillsList>
-        <SkillsList>
-          <OverlinedHeading>MORE</OverlinedHeading>
-          <SkillName primary>Googling</SkillName>
-          <SkillName primary>Responsive Design</SkillName>
-          <SkillName primary>Agile Methodology</SkillName>
-          <SkillName primary>Mob Programming</SkillName>
-          <SkillName primary>Indonesian</SkillName>
-          <SkillName primary>Swedish</SkillName>
-        </SkillsList>
+        {data.map((item) => (
+          <SkillsList key={item.id}>
+            <OverlinedHeading>{item.title}</OverlinedHeading>
+            {item.skills.map((skill) => (
+              <SkillName primary key={skill}>{skill}</SkillName>
+            ))}
+          </SkillsList>
+        ))}
+
       </SkillsContainer>
     </Wrapper>
   )
@@ -53,7 +30,6 @@ const SkillsContainer = styled.div`
   @media (min-width: 668px){
     grid-template-columns: repeat(3, 1fr);
   }
-
 `
 
 const SkillsList = styled.div`
