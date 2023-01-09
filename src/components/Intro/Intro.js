5cde555e7a6aca38becb9d3e2b44c412cb7f6ccd
SofieRydmark <you@example.com>
2023-01-09 09:02:38 +0100

    update intro

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -16,7 +16,7 @@ const Intro = () => {
           autoplay
           speed={1}
         />
-        Hello! I <span>&apos;</span>m Frontend Developer from Technigo<span>&apos;</span>s Web
+        Hello! I <span>&apos;</span>m a Frontend Developer from Technigo<span>&apos;</span>s Web
         developer bootcamp. I have worked for nearly 10 years in media and recently as a
         programmatic trader. I love that you can be so creative with code and I have a good eye for
         design and the use of CSS. I am a very fast learner that thrive in a team who help each
