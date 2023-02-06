b72aa7198341f8226ce76d4d932605200768640b
ViktorSvenssonN <viktor.tommy.svensson@gmail.com>
2023-02-06 21:27:29 +0100

    Updating ForMore icons

--- src/components/ForMore.js
+++ src/components/ForMore.js
@@ -58,7 +58,6 @@ const ForMoreBox = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
 
     @media (max-width: 800px) {
         width: 30%;
