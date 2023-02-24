97f89a025c7e363fb341fa5ec936ac1112d6c29f
MariaWellander <maria.wellander@gmail.com>
2023-02-24 18:55:56 +0100

    fixing Netlify deply error

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -7,9 +7,10 @@ export const Tech = () => {
       <TechInnerWrapper>
         <h5>TECH</h5>
         <p>HTML, CSS, Flexbox, CSS Grid, <span>JavaScript</span>, JSX,
-          <span> React</span>, React Hooks,<span> Redux</span>, 
-        Node.js, <span> API:s, JSON</span>, MongoDB, Mongoose, REST, Express, CORS, bcrypt, CryptoJS, 
-        Web Accessibility, mob-programming, pair-programming & GitHub.
+        <span> React</span>, React Hooks,<span> Redux</span>, 
+        Node.js, <span> API:s, JSON</span>, MongoDB, Mongoose, REST, Express, CORS, 
+        bcrypt, CryptoJS, Web Accessibility, mob-programming,
+        pair-programming & GitHub.
         </p>
       </TechInnerWrapper>
     </TechOuterWrapper>
