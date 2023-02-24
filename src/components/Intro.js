082bd52475a482f89e77875c86e758c8eb09faa7
MariaWellander <maria.wellander@gmail.com>
2023-02-24 21:47:08 +0100

    trying to fix error in deployment

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,11 +8,11 @@ export const Intro = () => {
         <p> Hello! As a recent graduate in Frontend Development,
           I bring a unique background in accounting and
           French international business and economics to the table.
-          I have a comprehensive understanding of the challenges faced by 
+          I have a comprehensive understanding of the challenges faced by
           both the finance and IT departments, allowing me to effectively
           communicate and collaborate with professionals from both fields.
           When coding, I always keep in mind the importance of
-          user-friendliness and the company's financial goals.
+          user-friendliness and the company\'s financial goals.
           With my skills and perspective, I am confident in my ability to
           create reliable and profitable software solutions.
         </p>
