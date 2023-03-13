742dd6c5d2e0807c4a75777c2e957ed2f2816521
a-well <amandaemkwellington@gmail.com>
2023-03-13 11:36:02 +0100

    Content update

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -16,7 +16,7 @@ const Summary = () => {
         <SummaryText>
           <Lollipop />
           Hi there! My name is Amanda and I like building neat looking things for the web.
-          I&apos;m based in Stockholm, Sweden and currently looking for a job :)
+          I&apos;m based in Stockholm, Sweden and currently looking for a job :D
         </SummaryText>
       </Wrapper>
     </PageSection>
