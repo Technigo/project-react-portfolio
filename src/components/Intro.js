c1936c24d765adb40db68433960add0b92cdca8f
Sarah Kneedler <kneedlersarah@gmail.com>
2023-03-14 14:13:58 +0100

    New intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,38 +1,40 @@
+/* eslint-disable quotes */
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
-import React from 'react'
-import styled from 'styled-components/macro';
-import { InnerWrapper, ClonedSectionWrapper } from './styles/MainStyle'
+import React from "react";
+import styled from "styled-components/macro";
+import { InnerWrapper, ClonedSectionWrapper } from "./styles/MainStyle";
 
 const Intro = () => {
   return (
     <ClonedSectionWrapper>
       <InnerWrapper>
         <p tabIndex="0">
-          <Line /><Dot />
-     Hello and welcome! Since the middle of August I&apos;m a student at
-     Technigo bootcamp, an intense and fun way to learn code. Before this
-     career change I studied and then practiced in the field of social work
-     with has giving me skills that can be valuable in the world of web
-     development. A few skills are creativity, to think outside the box and
-     be flexible.
+          <Line />
+          <Dot /> Hi! I&apos;m a curious junior frontend developer with a
+          background in social work. I&apos;m eager to continue learning and
+          I&apos;m passionate about combining creativity and logic to build
+          great and user-friendly products. Previous work experience has made me
+          structured, responsible and skilled in working in teams as well as
+          independently. I&apos;m a fast learner and i love finding solutions.
+          With me in your team you will have a social and dedicated team member!
         </p>
       </InnerWrapper>
     </ClonedSectionWrapper>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
 
-const Line = styled.span` 
-    border: 1px solid #4f6970;
-    width: 40px;
-    display: inline-block;
-    margin-bottom: 4px;
-    `
+const Line = styled.span`
+  border: 1px solid #4f6970;
+  width: 40px;
+  display: inline-block;
+  margin-bottom: 4px;
+`;
 const Dot = styled.span`
-    height: 10px;
-    width: 10px;
-    background-color: #4f6970;
-    border-radius: 50%;
-    display: inline-block;
-    `
+  height: 10px;
+  width: 10px;
+  background-color: #4f6970;
+  border-radius: 50%;
+  display: inline-block;
+`;
