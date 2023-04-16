b31a3eab5cbb86eed9bd48344dfe048514c2bc68
carimore <110664183+carimore@users.noreply.github.com>
2023-04-16 19:21:24 +0200

    Change Text

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,9 +5,10 @@ const Intro = () => {
   return (
     <SectionContainer>
       <Text>
-        I am a creative web designer and frontend developer with a background in education.
-        My goal is to use my problem solution mindset and knowledge about learning,
-        to help entrepreneurs and small businesses create amazing websites.
+        I am a web designer and front-end developer with a background in education.
+        My goal is to use my creativity and problem solution mindset to help entrepreneurs design
+        amazing personalized websites. In the process I include training hours to help clients
+        learn how to use the website platform and make changes by themselves.
       </Text>
     </SectionContainer>
 
