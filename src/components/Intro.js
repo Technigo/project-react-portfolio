c99d18ac8bbdc3fecee5e684c1e84db2b78a6e43
carimore <110664183+carimore@users.noreply.github.com>
2023-04-16 19:24:24 +0200

    Changes 3

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,7 +8,7 @@ const Intro = () => {
         I am a web designer and front-end developer with a background in education.
         My goal is to use my creativity and problem solution mindset to help entrepreneurs and
         small business design amazing personalized websites. In the process I include training
-        hours to help clients learn how to use the website platform and make changes by themselves.
+        hours to teach clients how to use the website platform and make changes by themselves.
       </Text>
     </SectionContainer>
 
