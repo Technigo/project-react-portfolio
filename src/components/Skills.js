e203bab669c650326a11fde210771fe41593fa3c
Andrea <andrea@dhcp-10-187-159-124.guest.stockholm.se>
2023-04-28 20:08:15 +0200

    added tech-skills

---
+++ src/components/Skills.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <Header>
+      <h1>Skills</h1>
+    </Header>
+    <TechSkills>
+        <Code>
+            <span>Code</span>
+
+</p>
+        </Code>
+        <ToolBox>
+
+        </ToolBox>
+        <More>
+
+        </More>
+        <Upcoming>
+
+        </Upcoming>
+    </TechSkills>
+  )
+}
+
+export default Skills;
