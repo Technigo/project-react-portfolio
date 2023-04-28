1e2e392f51c5403965f9ae372d906a1151f6598a
Andrea <andrea@dhcp-10-187-159-124.guest.stockholm.se>
2023-04-28 20:44:13 +0200

    skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,27 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Skills = () => {
   return (
-    <Header>
-      <h1>Skills</h1>
-    </Header>
-    <TechSkills>
+    <SkillSection>
+      <Header>
+        <h1>Skills</h1>
+      </Header>
+      <TechSkills>
         <Code>
-            <span>Code</span>
-
-</p>
+          <ul>
+            <li><span>Code</span></li> 
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Styled Components</li>
+          </ul>
         </Code>
         <ToolBox>
-
+          <ul>
+            <li><span>Toolbox</span></li>
+            <li>Visual Studio Code</li>
+            <li>Figma</li>
+            <li>Slack</li>
+            <li>Canva</li>
+            <li>Github</li>
+          </ul>
         </ToolBox>
         <More>
-
+          <ul>
+            <li><span>More</span></li>
+            <li>Design lead</li>
+            <li>Branding</li>
+          </ul>
         </More>
         <Upcoming>
-
+          <li><span>Upcoming</span></li>
+          <li>Node.js</li>
         </Upcoming>
-    </TechSkills>
+      </TechSkills>
+    </SkillSection>
   )
 }
 
 export default Skills;
+
+const SkillSection = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`
